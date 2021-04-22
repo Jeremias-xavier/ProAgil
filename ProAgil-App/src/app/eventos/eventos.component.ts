5b4a7af3d257be26f122fe38f10e8e8e39902c4c
Jeremias Xavier <Jeremias.lourenco@gmail.com>
2021-04-22 07:01:30 -0300

    AddProjetos

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { empty } from 'rxjs';
 
 @Component({
   selector: 'app-eventos',
@@ -7,8 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.css']
 })
 export class EventosComponent implements OnInit {
-  eventos: any = {};
-  filtroLista = '';
+  eventos: any = [];
+  eventosFiltrados: any = [];
+  _filtroLista: string = "";
+
+  get filtroLista(): string{
+    return this._filtroLista;
+  }
+  set filtroLista(value: string){
+   this._filtroLista = value;
+   this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -23,6 +34,13 @@ export class EventosComponent implements OnInit {
     }, error => {
       console.log(error);
     });
+  }
+
+  filtrarEventos(filtrarpor: string): any{
+    filtrarpor = filtrarpor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarpor) !== -1
+    );
   }
 
 }
